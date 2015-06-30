5f6874a206790bd5b6655c4fd301641b833fa280
mylinsz <504685302@qq.com>
2015-07-01 00:14:30 +0800

    元素标签、ID和类(class)选择器
    元素个数：属性length、方法size()；获取某个元素：eq(下标)

--- selector.js
+++ selector.js
@@ -2,6 +2,20 @@
 //延时加载
 $(function () {
 	
-  $('#box').css('color', 'blue'); //添加一个行为
+  //$('#box').css('color', 'red'); //添加一个行为
+  
+  //$('div').css('color', 'green');
+  
+  //alert($('div').length);
+  
+  //alert($('#box').length);
+  
+  //jQuery核心自带的一个属性length或size()方法用于查看返回的元素个数
+  //alert($('.box').length); 
+  //alert($('.box').size());
+  
+  $('div').eq(1).css('color', 'red');
+  
+  $('.box').eq(0).css('color', 'green');
   
 });
