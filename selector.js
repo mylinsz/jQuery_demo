f07a4d0a82739804c5531fa4d1f7aa62672b8b5f
mylinsz <504685302@qq.com>
2015-07-01 14:14:09 +0800

    【简单选择器】通过数组下标也可以获取DOM对象

--- selector.js
+++ selector.js
@@ -1,21 +1,47 @@
-﻿
+
 //延时加载
 $(function () {
-	
-  //$('#box').css('color', 'red'); //添加一个行为
+	/*
+  $('#box').css('color', 'red'); //添加一个行为
   
-  //$('div').css('color', 'green');
+  $('div').css('color', 'green');
   
-  //alert($('div').length);
+  alert($('div').length);
   
-  //alert($('#box').length);
+  alert($('#box').length);
   
-  //jQuery核心自带的一个属性length或size()方法用于查看返回的元素个数
-  //alert($('.box').length); 
-  //alert($('.box').size());
+  jQuery核心自带的一个属性length或size()方法用于查看返回的元素个数
+  alert($('.box').length); 
+  alert($('.box').size());
   
   $('div').eq(1).css('color', 'red');
-  
   $('.box').eq(0).css('color', 'green');
+  
+  $('.pox').css('color', 'green');
+  $('#box').css('color', 'green');
+  
+  if(document.getElementById('pox')){
+	  document.getElementById('pox').style.color = 'red';
+  }
+  
+  if(document.getElementById('box')){
+	  document.getElementById('box').style.color = 'red';
+  }
+  
+  if($('#box').size() > 0){  //$('#box').length
+	  $('#box').css('color', 'green');
+  }
+  if($('#pox').size() > 0){
+	  $('#pox').css('color', 'green');
+  }
+  
+  */
+  
+  //通过数组下标获取DOM对象 ：$('#box').get(0) 和 $('#box')[0]
+  if($('#box')[0]){
+	  //$('#box').get(0).css('color', 'green'); //这种写法不可以
+	  //$('#box').eq(0).css('color', 'green'); //这种写法可以
+	  $('#box').css('color', 'green');
+  }
   
 });
